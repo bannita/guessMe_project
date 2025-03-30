5bc452e9088bb7564d5711468150453166c37f3b
bannita <anibidzinashvili98@gmail.com>
2025-03-30 23:57:00 +0400

    Connected Flask to Docker PostgreSQL with environment variables

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,24 @@
 #Starts the Flask app and registers routes from other files.
-
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
 
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+
+app.config['SQLALCHEMY_DATABASE_URI'] = ( f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
 @app.route("/")
 def home():
-    return "🚀 Hello"
+    return " hi kitty ^-^"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
